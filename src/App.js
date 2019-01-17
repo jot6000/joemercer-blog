59d1f9130e3ccf70c51f3f1819e60ffb444c2039
Joe Mercer <joemercer50@googlemail.com>
2019-01-17 13:49:18 +0000

    Set up basic project with React Router and Create react app

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import About from './components/pages/About'
+import Home from './components/pages/Home'
+import Posts from './components/pages/Posts'
+import Projects from './components/pages/Projects'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <>
+          <div className="App-header">
+            <Link to="/" className="Nav-home">
+              Joe Mercer's React Blog
+              </Link>
+          </div>
+          <div>
+            <div className="App-navBar">
+              <Link className="Nav-Button" to="/about">About</Link>
+              <Link className="Nav-Button" to="/posts">Posts</Link>
+              <Link className="Nav-Button" to="/projects">Projects</Link>
+            </div>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/projects" component={Projects} />
+          </div>
+        </>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/pages/Posts.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Posts.css';
+
+class Posts extends Component {
+  render() {
+    return (
+      <div>
+      <h2>Posts</h2>
+    </div>
+    );
+  }
+}
+
+export default Posts;
