7de41772475eeecf89b5aa9bb2b24ee5cc80ddcb
Joe Mercer <joemercer50@googlemail.com>
2019-01-17 14:05:29 +0000

    Basic homepage template

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,9 +4,14 @@ import './Home.css';
 class Home extends Component {
   render() {
     return (
-      <div>
-      <h2>Home</h2>
-    </div>
+      <div className='container'>
+        <h1>Welcome</h1>
+        <p>This blog is intended to house my proffesional portfolio as well as be a home for some of my more artistic endevours.</p>
+        <p>Coming here for the first time I recomend you look at the below links first.</p>
+        <div className='links'>1.Lorem</div>
+        <div className='links'>2.Ipsum</div>
+        <div className='links'>3.Dolar</div>
+      </div>
     );
   }
 }
