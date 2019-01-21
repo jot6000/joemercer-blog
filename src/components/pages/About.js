2616e07dcce3f562576176eba0c7458cb34f182c
Joe Mercer <joemercer50@googlemail.com>
2019-01-21 02:55:04 +0000

    Sorted out links on about page properly.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -23,10 +23,19 @@ class About extends Component {
                     Because of this I will be posting about writing and world building as well as putting my stories up here.
                 </p>
                 <h4>Other Profiles</h4>
-                <div>
-                    <a style={{padding:'1rem'}} href="https://github.com/jot6000">GitHub</a>
-                    <a style={{padding:'1rem'}} href="https://www.linkedin.com/in/joe-mercer-214145149/">LinkedIn</a>
-                    <a style={{padding:'1rem'}} href="https://www.wattpad.com/user/JoeMercer0">WattPad</a>
+                <div className='profile-footer'>
+                    <div className='profileContainer'>
+                        <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1024px-Octicons-mark-github.svg.png"/>
+                        <a className='link-text' href="https://github.com/jot6000">GitHub</a>
+                    </div>
+                    <div className='profileContainer'>
+                    <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"/>
+                        <a className='link-text' href="https://www.linkedin.com/in/joe-mercer-214145149/">LinkedIn</a>
+                    </div>
+                    <div className='profileContainer'>
+                    <img className='logo' src="https://vignette.wikia.nocookie.net/thedescendants/images/8/85/Wattpad-logo.png/revision/latest?cb=20170710213216"/>
+                        <a className='link-text' href="https://www.wattpad.com/user/JoeMercer0">WattPad</a>
+                    </div>
                 </div>
             </div>
         );
