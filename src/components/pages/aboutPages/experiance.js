6b2a86b8cc05a5a0a93f10f449b3095fc151dd83
Joe Mercer <joemercer50@googlemail.com>
2019-04-24 00:54:18 +0100

    Rough out design of education part of experiance

--- src/components/pages/aboutPages/experiance.js
+++ src/components/pages/aboutPages/experiance.js
@@ -6,15 +6,42 @@ class Experiance extends Component {
     render() {
         return (
             <div className='container'>
-                <br/>
+                <br />
                 <div className='about-header'>Qualifications and Experiance</div>
-                <br/>
-                <p className='aboutText'>
-                    From the title of this blog you may have already guessed that my name is Joe Mercer and I am a Juinor Software Engineer.
-                    I started off studying games development through college but decided from the process that I generally enjoyed the programming aspects more than
-                    the artsy side, and as such, have not persued games development profesionaly.
-                </p>
-                <Footer/>
+                <br />
+                <div style={{color:"#303030",fontSize:'26pt',marginBottom:'10px'}}>
+                    1.Education
+                </div>
+                <div style={{display:'flex',marginBottom:'18px'}}>
+                    <div style={{boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)", display:'flex', flexDirection: 'column', alignItems: "center",padding:"10px", width:'30vw',marginRight:'2vw'}}>
+                        <div style={{fontSize:"16pt", marginBottom:'10px'}}>HND level 5, Games Development</div>
+                        <div style={{ height: '1px', background: 'grey',width:'100%'}} />
+                        <div style={{fontSize:"12pt", marginBottom:'10px',fontFamily: 'Open Sans, sans-serif'}}>October 2015 - July 2017</div>
+                        <div style={{fontSize:"12pt", marginBottom:'10px',fontFamily: 'Open Sans, sans-serif'}}>Uxbridge College, Uxbridge, London</div>
+                        <div>Grade:</div>
+                        <div>Distinction</div>
+                    </div>
+                    <div style={{boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)", display:'flex', flexDirection: 'column', alignItems: "center",padding:"10px", width:'30vw',marginRight:'2vw'}}>
+                        <div style={{fontSize:"16pt", marginBottom:'10px'}}>BTEC level 3 Extended Diploma, Games Development</div>
+                        <div style={{ height: '1px', background: 'grey',width:'100%'}} />
+                        <div style={{fontSize:"12pt", marginBottom:'10px',fontFamily: 'Open Sans, sans-serif'}}>September 2013 - June 2015.</div>
+                        <div style={{fontSize:"12pt", marginBottom:'10px',fontFamily: 'Open Sans, sans-serif'}}>Uxbridge College, Uxbridge, London</div>
+                        <div>Grade:</div>
+                        <div>Distinction*, Distinction, Distinction</div>
+                    </div>
+                    <div style={{boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)", display:'flex', flexDirection: 'column', alignItems: "center",padding:"10px", width:'30vw'}}>
+                        <div style={{fontSize:"16pt", marginBottom:'10px'}}>GCSE’s</div>
+                        <div style={{ height: '1px', background: 'grey',width:'100%'}} />
+                        <div style={{fontSize:"12pt", marginBottom:'10px',fontFamily: 'Open Sans, sans-serif'}}>September 2011 – June 2013</div>
+                        <div style={{fontSize:"12pt", marginBottom:'10px',fontFamily: 'Open Sans, sans-serif'}}>Chalfont Community College, Chalfont St Peter, Buckinghamshire</div>
+                        <div>Grade:</div>
+                        <div>10 GCSEs, including English, Maths and Science at grades A*-C</div>
+                    </div>
+                </div>
+                <div style={{color:"#303030",fontSize:'26pt',marginBottom:'10px'}}>
+                    2.Career
+                </div>
+                <Footer />
             </div>
         );
     }
