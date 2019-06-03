1add0f778cb59df0b493fc3ce6c85ea4ad380d57
Joe Mercer <joemercer50@googlemail.com>
2019-06-03 11:46:43 +0100

    Added modal for extra details in career cards

--- src/components/cards/CareerCard.js
+++ src/components/cards/CareerCard.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
+import Modal from 'react-responsive-modal'; 
 import './ExperianceCards.css';
 
 class CareerCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+    openModal = () => {
+        this.setState({ open: true });
+      };
+     
+      onCloseModal = () => {
+        this.setState({ open: false });
+      };
     render() {
         return (
             <div className="experiance-card">
+                <Modal
+                    open={this.state.open}
+                    onClose={this.onCloseModal}
+                    center
+                >
+                    <h3 className={'experiance-title'}>Overview</h3>
+                    <div className={'experiance-details'}>{this.props.overview}</div>
+                    <h3 className={'experiance-title'}>Responsibilities</h3>
+                    {this.props.responsibilities.map((responsibility)=>
+                      <li className={'experiance-details'}>{responsibility}</li>
+                    )}
+                    <h3 className={'experiance-title'}>Technologies used</h3>
+                    {this.props.tech.map((technoligy)=>
+                      <li className={'experiance-details'}>{technoligy}</li>
+                    )}
+                </Modal>
                 <div className="experiance-title">{this.props.title}</div>
                 <div className="experiance-divider"/>
                 <div className="experiance-details">{this.props.jobTitle}</div>
                 <div className="experiance-details">{this.props.date}</div>
                 <div className="experiance-details">{this.props.location}</div>
                 <div className="experiance-divider-small"/>
-                <div className="experiance-more">Click for more details</div>
+                <div className="experiance-more"
+                    onClick={this.openModal}
+                >
+                    Click for more details
+                </div>
             </div>
         )
     }
